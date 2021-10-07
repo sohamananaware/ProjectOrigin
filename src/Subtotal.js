36a583b81f45428ebf2912b339f9efb0cde7becd
soham nanaware <sohamananaware@gmail.com>
2021-10-07 11:43:07 +0530

    Commit

---
+++ src/Subtotal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Subtotal.css'
+import CurrencyFormat from 'react-currency-format'
+
+function Subtotal() {
+    return (
+        <div className='subtotal'>
+            <CurrencyFormat 
+            
+            />
+        </div>
+    )
+}
+
+export default Subtotal
